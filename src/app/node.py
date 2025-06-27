547097275e7106a775ef98af3a8ae5a9a5156079
Ankesh2004 <gankesh808@gmail.com>
2025-06-27 08:52:51 +0530

    feat: get random nodes logic

--- src/app/node.py
+++ src/app/node.py
@@ -7,6 +7,7 @@ from singleton import Singleton
 import logging
 import hashlib
 import json
+import secrets
 
 
 @Singleton
@@ -30,3 +31,26 @@ class Node:
         self.session_to_monitoring = requests.Session()
         self.push_mode = None
         self.is_send_data_back = None
+
+    def set_params(self, ip, port, cycle, node_list, data, is_alive, gossip_counter, failure_counter,
+                   monitoring_address, database_address, is_send_data_back, client_thread, counter_thread, data_flow_per_round, push_mode, client_port):
+        self.ip = ip
+        self.port = port
+        self.monitoring_address = monitoring_address
+        self.database_address = database_address
+        self.cycle = cycle
+        self.node_list = node_list
+        self.data = data
+        self.is_alive = is_alive
+        self.gossip_counter = gossip_counter
+        self.failure_counter = failure_counter
+        self.client_thread = client_thread
+        self.counter_thread = counter_thread
+        self.data_flow_per_round = data_flow_per_round
+        self.is_send_data_back = is_send_data_back
+        self.push_mode = push_mode
+        self.client_port = client_port
+
+    def get_random_nodes(self, node_list, target_count):
+        filtered_nodes = [node for node in node_list if node['ip'] != self.ip]
+        return secrets.SystemRandom().sample(filtered_nodes, target_count)
