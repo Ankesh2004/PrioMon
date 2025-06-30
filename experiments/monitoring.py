fa7100d5922eab6d948929df6fb0ddfa9ad90cb9
Ankesh2004 <gankesh808@gmail.com>
2025-06-30 15:06:08 +0530

    feat: implement Docker node lifecycle management functions

--- experiments/monitoring.py
+++ experiments/monitoring.py
@@ -34,7 +34,7 @@ experiment = None
 def execute_queries_from_queue():
     while True:
         try:
-            conn = sqlite3.connect('demonDB.db', check_same_thread=False)
+            conn = sqlite3.connect('priomonDB.db', check_same_thread=False)
             cursor = conn.cursor()
             query_data = experiment.query_queue.get()
             if query_data is None:
@@ -119,6 +119,76 @@ class Experiment:
 
     def set_db_id(self, param):
         self.db_id = param
+
+def spawn_node(index, node_list, client, custom_network_name):
+    try:
+        new_node = docker_client.containers.run("demonv1", auto_remove=True, detach=True,
+                                                network_mode=custom_network_name,
+                                                ports={'5000': node_list[index]["port"]})
+    except Exception as e:
+        print("Node not spawned: {}".format(e))
+        print("trace: {}".format(traceback.format_exc()))
+        node_list[index]["port"] = get_free_port()
+        spawn_node(index, node_list, client, custom_network_name)
+    else:
+        node_details = client.containers.get(new_node.id)
+        node_list[index] = {"id": node_details.id,
+                            "ip": node_details.attrs['NetworkSettings']['Networks']['test']['IPAddress'],
+                            "port": node_details.attrs['NetworkSettings']['Ports']['5000/tcp'][0]['HostPort']}
+
+def spawn_multiple_nodes(run):
+    network_name = "test"
+    from_index = 0
+    if run.node_list is None:
+        run.node_list = [None] * run.node_count
+    elif len(run.node_list) == run.node_count:
+        return  # Nodes are already spawned
+    else:
+        from_index = len(run.node_list)
+        run.node_list = run.node_list + [None] * (run.node_count - len(run.node_list))
+    client = docker.DockerClient()
+    # TODO: free ports on i
+    for i in range(from_index, run.node_count):
+        run.node_list[i] = {}
+        run.node_list[i]["port"] = get_free_port()
+    Parallel(n_jobs=-1, prefer="threads")(
+        delayed(spawn_node)(i, run.node_list, client, network_name) for i in range(from_index, run.node_count))
+
+def nodes_are_ready(run):
+    for i in range(0, run.node_count):
+        if docker_client.containers.get(run.node_list[i]['id']).status != "running":
+            return False
+        run.node_list[i]["is_alive"] = True
+    return True
+
+def restart_node(docker_id):
+    try:
+        docker_client.containers.get(docker_id).restart()
+    except Exception as e:
+        print("An error occurred while restarting the container: {}".format(e))
+
+def reset_node(ip, port, docker_id):
+    try:
+        time.sleep(random.uniform(0.01, 0.05))
+        requests.get("http://{}:{}/reset_node".format(ip, port), timeout=30)
+    except Exception as e:
+        print("An error occurred while sending the request: {}".format(e))
+        restart_node(docker_id)
+
+@monitoring_priomon.route('/delete_nodes', methods=['GET'])
+def delete_all_nodes():
+    to_remove = docker_client.containers.list(filters={"ancestor": "demonv1"})
+    for node in to_remove:
+        node.remove(force=True)
+    return "OK"
+
+@monitoring_priomon.route('/restart_all', methods=['GET'])
+def restart_all_nodes(run):
+    start = time.time()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=run.node_count) as executor:
+        for i in range(0, run.node_count):
+            executor.submit(restart_node, run.node_list[i]["id"])
+    print("Restart time: {}".format(time.time() - start), flush=True)
 
 if __name__ == "__main__":
     monitoring_priomon.run(host='0.0.0.0', port=4000, debug=False, threaded=True)
